b3f74a3fd4e38ce6b44f9adfc7ff222c62a84b5f
Swapnil11112003 <143048873+Swapnil11112003@users.noreply.github.com>
2026-08-23 16:50:51 -0500

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = " ".join(filter(str.isalnum, s)).lower()
+
+        i = 0
+        j = len(new_s) - 1
+        while i < j:
+            if new_s[i] != new_s[j]:
+                return False
+            
+            i += 1
+            j -=1
+
+        return True
+
+        
+
+        
